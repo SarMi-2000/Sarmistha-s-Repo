cb83446ff82a4850c9eb9ac768b8712688b0daf8
Sarmistha Bera <sarmisthabera19990@gmail.com>
2023-01-11 16:09:06 +0530

    added pov

--- Views/hungry.py
+++ Views/hungry.py
@@ -1,8 +1,10 @@
 hungry=input("are you hungry?")
-if hungry=="yes":
+if hungry=="yes" and hungry=="yo":
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
+    print("Now, we have to exercise")
 else:
     print("do your homework")
     print("eat fries")
+    print("I want to Dance now")
